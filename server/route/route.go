cdb5efe31b6318cfb7f1b2f0a5b530c4e4eb35d2
linhhd <linhhd@vng.com.vn>
2021-03-26 14:00:20 +0700

    add route

--- server/route/route.go
+++ server/route/route.go
@@ -16,13 +16,17 @@ func Routes() *mux.Router {
 
 	test := r.Methods("GET").Subrouter()
 	test.HandleFunc("/hello", handler.Test)
-	test.Use(acl.DisallowAnon)
+	// test.Use(acl.DisallowAnon)
 
 	//User
 	r.HandleFunc("/users", handler.RegisterUser).Methods(http.MethodPost)
 	// r.HandleFunc("/users", handler.GetAllUser)
 	r.HandleFunc("/users/by", handler.GetUserByField).Methods(http.MethodGet)
 	r.HandleFunc("/users/by-email", handler.UpdateUserByEmail).Methods(http.MethodPut)
+
+	uByCookie := r.Methods(http.MethodGet).Subrouter()
+	uByCookie.HandleFunc("/users/by-cookie", handler.GetUserByCookie)
+	uByCookie.Use(acl.DisallowAnon)
 
 	//Product
 	r.HandleFunc("/products/{id}", handler.GetProductByID).Methods("GET")
@@ -36,6 +40,16 @@ func Routes() *mux.Router {
 	r.HandleFunc("/categories", handler.CreateNewCategory).Methods("POST")
 	r.HandleFunc("/categories", handler.DeleteCategory).Methods("DELETE")
 	r.HandleFunc("/categories", handler.UpdateCategory).Methods("PUT")
+
+	//Login
+	login := r.Methods("POST").Subrouter()
+	login.HandleFunc("/login", handler.Login)
+	login.Use(acl.DisallowAuth)
+
+	//logout
+	logout := r.Methods(http.MethodGet).Subrouter()
+	logout.HandleFunc("/logout", handler.Logout)
+	logout.Use(acl.DisallowAnon)
 
 	return r
 }
