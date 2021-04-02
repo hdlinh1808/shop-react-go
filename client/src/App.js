a4c2f78cccc36479a91060f4331184da1e71e49f
linhhd <linhhd@vng.com.vn>
2021-04-02 17:17:18 +0700

    update App.js change route contruct (a little)

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ import './styles/sidebar.css'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AdminPage from './components/admin/AdminPage';
-import UserPage from './components/UserPage';
+
+import { Container} from "semantic-ui-react";
+import AppHeader from './components/AppHeader';
+import AppBody from './components/AppBody';
+import AppFooter from './components/AppFooter';
 
 
 function App() {
@@ -15,9 +19,16 @@ function App() {
                 <Route path="/admin">
                     <AdminPage />
                 </Route>
-                <Route path="/">
-                    <UserPage></UserPage>
-                </Route>
+
+                {/* <Route path="/"> */}
+                    {/* <UserPage></UserPage> */}
+                {/* </Route> */}
+
+                <Container>
+                    <AppHeader></AppHeader>
+                    <AppBody></AppBody>
+                </Container>
+                <AppFooter></AppFooter>
             </Switch>
         </Router>
     );
