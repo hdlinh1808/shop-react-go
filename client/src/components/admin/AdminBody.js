3789ff04294712643e3d65b86501a0a4af6f2c95
linhhd <linhhd@vng.com.vn>
2021-04-02 17:20:09 +0700

    update admin body add title and breadcrumb

--- client/src/components/admin/AdminBody.js
+++ client/src/components/admin/AdminBody.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react'
-import { Grid, Sidebar, Segment } from 'semantic-ui-react'
-import { Switch, Route, withRouter } from 'react-router-dom'
+import { Header, Icon, Segment, Message, Breadcrumb } from 'semantic-ui-react'
+import { Switch, Route, withRouter, Link } from 'react-router-dom'
 import Dashboard from './Dashboard'
 import ProductList from './product/ProductList'
 import { matchPath } from 'react-router-dom';
+import Category from './category/Category'
 
 export class AdminBody extends Component {
     constructor(props) {
         super(props)
-    
-        this.state = {
-             
-        }
 
+        this.state = {
+            title: "",
+            breadCrumbs: [],
+        }
     }
-    
+
+    setTitle(title) {
+        this.setState({
+            title: title,
+        })
+    }
+
+    setBreadCrumbs(breadCrumbs) {
+        if (breadCrumbs != null) {
+            this.setState({
+                breadCrumbs: breadCrumbs,
+            })
+        }
+    }
+
+    renderComponent(Component) {
+        return <Component setTitle={(title) => this.setTitle(title)} setBreadCrumbs={(breadCrumbs) => this.setBreadCrumbs(breadCrumbs)} />
+    }
 
     render() {
-        return (
+        let relative = this.props.match.path
+        let breadCrumbs = null;
+
+        breadCrumbs = this.state.breadCrumbs.map(b => {
+            if (b.link)
+                return <Breadcrumb.Section as={Link} to={b.to} link={b.link}>{b.content}</Breadcrumb.Section>
+            return <Breadcrumb.Section active>{b.content}</Breadcrumb.Section>
+        })
+
+        if (this.state.breadCrumbs?.length >= 1) {
+            breadCrumbs = breadCrumbs.reduce((prev, cur) => <>{prev}{<Breadcrumb.Divider />}{cur}</>)
+            breadCrumbs = <Breadcrumb>{breadCrumbs}</Breadcrumb>
+        }
+        return (   
             <Segment className="content">
-                <Switch>
-                    <Route path={`${this.props.match.path}`} exact component={Dashboard}></Route>
-                    <Route path={`${this.props.match.path}/product/list`} component={ProductList}></Route>
-                </Switch>
+                <Message>
+                    <Header as='h3'>
+                        <Header.Content>{this.state.title}</Header.Content>
+                    </Header>
+                </Message>
+                {breadCrumbs}
+                <Segment>
+                    <Switch>
+                        <Route path={`${relative}`} exact render={(props) => this.renderComponent(Dashboard)} />
+                        <Route path={`${relative}/product/list`} exact render={(props) => this.renderComponent(ProductList)} />
+                        <Route path={`${relative}/category`} exact render={(props) => this.renderComponent(Category)} />
+                    </Switch>
+                </Segment>
             </Segment>
         )
     }
